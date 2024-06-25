3774c37a7010169577df39a613a87961fb87048b
Andrei ALDEA <aldea.andrei.977@gmail.com>
2024-06-25 12:00:25 +0300

    git: ignore ide files

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include "raylib.h"
 
+
+
 int main(void)
 {
-     const int screenWidth = 800;
+    const int screenWidth = 800;
     const int screenHeight = 450;
 
     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
@@ -12,11 +14,8 @@ int main(void)
     while (!WindowShouldClose())
     {
         BeginDrawing();
-
             ClearBackground(RAYWHITE);
-
             DrawText("Congrats! You created your first window!", 190, 200, 20, LIGHTGRAY);
-
         EndDrawing();
     }
     CloseWindow();
